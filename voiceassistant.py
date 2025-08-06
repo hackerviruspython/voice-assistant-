71526241e8eb2031571c28e16a391559ec485c01
hackerviruspython <ykvermavirus@gmail.com>
2025-08-06 10:10:25 +0530

    Add files via upload

---
+++ voiceassistant.py
@@ -0,0 +1,36 @@
+import pyttsx3
+import speech_recognition as sr
+def take_commands():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening')
+        r.pause_threshold = 0.7
+        audio = r.listen(source)
+        try:
+            print("Recognizing")
+            Query = r.recognize_google(audio)
+            print("the query is printed='", Query, "'")
+        except Exception as e:
+            print(e)
+            print("Say that again sir")
+            return "None"
+    return Query
+def Speak(audio):
+    engine = pyttsx3.init()
+    engine.say(audio)
+    engine.runAndWait()
+if __name__ == '__main__':
+    while True:
+        command = take_commands()
+        if "exit" in command:
+            Speak("Sure sir! as your wish!")
+            break
+        if "Institute" in command:
+            Speak("Shivaye Classes is a research based skill provider institute")
+        if "learn" in command:
+            Speak("You can learn 300+ international courses")
+        if "thanks" in command:
+            Speak("You're most welcome")
+        if "hello" in command:
+                Speak("Tum kaise ho")
+
